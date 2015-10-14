ab3bcd47cf9fa579537884a393bbd0d2acf98db1
cheongyong <mcy1000@naver.com>
2015-10-15 03:19:52 +0900

    여러가지 추가용

--- IBLE/app/src/main/java/com/isoft/ible/BirthActivity.java
+++ IBLE/app/src/main/java/com/isoft/ible/BirthActivity.java
@@ -1,18 +1,75 @@
 package com.isoft.ible;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.Toast;
+
+import java.util.Calendar;
 
 
-public class BirthActivity extends ActionBarActivity {
+public class BirthActivity extends ActionBarActivity implements View.OnClickListener {
+
+    Button next,male,female,birth;
+    private String selectGender = "";
+    private Calendar mCalen;
+    private int day;
+    private int month;
+    private int year;
+    String strColor = "#fe4c4c";
+
+    private static final int DATE_PICKER_ID = 1111;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_birth);
+
+        next = (Button) findViewById(R.id.nextbtn);
+        male = (Button) findViewById(R.id.manbtn);
+        female = (Button) findViewById(R.id.womanbtn);
+        birth = (Button) findViewById(R.id.birthBtn);
+        next.setOnClickListener(this);
+        mCalen = Calendar.getInstance();
+        day = mCalen.get(Calendar.DAY_OF_MONTH);
+        month = mCalen.get(Calendar.MONTH);
+        year = mCalen.get(Calendar.YEAR);
+
+        birth.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDialog(DATE_PICKER_ID);
+            }
+        });
+
+        View.OnClickListener listener = new View.OnClickListener() {
+            public void onClick(View v) {
+
+                switch (v.getId()) {
+                    case R.id.manbtn://male
+                        male.setBackgroundResource(R.drawable.man_button_on);
+                        female.setBackgroundResource(R.drawable.woman_button_off);
+                        break;
+                    case R.id.womanbtn://female
+                        male.setBackgroundResource(R.drawable.man_button_off);
+                        female.setBackgroundResource(R.drawable.woman_button_on);
+                        break;
+                }
+            }
+        };
+        male.setOnClickListener(listener);
+        female.setOnClickListener(listener);
+
+
     }
+
 
 
     @Override
@@ -36,4 +93,34 @@ public class BirthActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    @Override
+    public void onClick(View v) {
+        Toast.makeText(getApplicationContext(),"oo",Toast.LENGTH_SHORT).show();
+        Intent i = new Intent(BirthActivity.this, LocationActivity.class);
+
+        startActivity(i);
+        finish();
+    }
+
+    @Override
+    protected Dialog onCreateDialog(int id) {
+        switch (id){
+            case DATE_PICKER_ID:
+                return new DatePickerDialog(this, pickerListener,year,month,day);
+        }
+        return null;
+    }
+
+    private DatePickerDialog.OnDateSetListener pickerListener = new DatePickerDialog.OnDateSetListener() {
+        public void onDateSet(DatePicker view, int selectedYear,
+                              int selectedMonth, int selectedDay) {
+            year = selectedYear;
+            month = selectedMonth;
+            day = selectedDay;
+            //birth.setTextColor(Color.parseColor(strColor));
+            birth.setText(new StringBuilder().append(year).append("-").append(month + 1)
+                    .append("-").append(day).append(" "));
+        }
+    };
 }
